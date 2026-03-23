2ae8f2295790bc741d5e07c738e20128dd617fee
David Todorovski <todorovskidavid23@gmail.com>
2026-03-23 17:54:47 +0100

    class04

---
+++ Class04/Class04.Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿//methodi interpolacija i metodi DaeTime tip vo C#
+
+
+//Methodi ili co class i funkcijata se vika metodata (methods)
+//pak sme na nivo klass Program.cs Program.csharp
+//nivo na klasa
+//TOP LEVEL STATEMENTS executable program si ima vo pozadina program.cs 
+//na nivo na klasa sme .NET znae deka sme nie vo klasa
+//
+//Zosto ni se dobri metodite
+//metoda e resuable peace code odredena logika sto treba na povekje mesta
+//Decouple responsibilitiies
+//Karakteristicno za STRONGLY TYPED LANGUAGE
+//nacin i pravilo na posuvanje
+//moraat da imaat return type
+//A method name mora da imame ime na metodata 
+//Parametars type tip na input parameri deklarirame metoda mora da znaeme od koj  tip se
+//void ako ne vrakja nisto mora da vrakja void taa e oznaka za nisto.
+
