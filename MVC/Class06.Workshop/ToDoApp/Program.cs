dc1ee01fe920edb9b3697db24328c88214e8022c
David Todorovski <todorovskidavid23@gmail.com>
2026-07-03 20:52:44 +0200

    DI added

--- MVC/Class06.Workshop/ToDoApp/Program.cs
+++ MVC/Class06.Workshop/ToDoApp/Program.cs
@@ -1,7 +1,20 @@
+using ToDoApp.DataAccess.Implementations;
+using ToDoApp.DataAccess.Interfaces;
+using ToDoApp.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//ovde se pravi Dependency Injections for repository
+builder.Services.AddScoped<IRepository<ToDo>, ToDoRepository>();
+//prvo sto da zeme koe repository , koja implemetacija da ja zeme
+builder.Services.AddScoped<IRepository<Category>, CategoryRepository>();
+builder.Services.AddScoped<IRepository<Status>, StatusRepository>();
+
+
+
 
 var app = builder.Build();
 
