a3eece51b7f433907f2adf66a485ad0c31e56d93
David Todorovski <todorovskidavid23@gmail.com>
2026-07-03 20:47:42 +0200

    Class06 Done

---
+++ MVC/Class06.Workshop/ToDoApp.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using ToDoApp.Domain;
+
+namespace ToDoApp.DataAccess.Interfaces
+{
+    //CRUD methods for the repository pattern (for accessing the db)
+    public interface IRepository<T> where T : BaseEntity
+    {
+        //CRUD
+        List<T> GetAll(); //Read all
+        T GetById(int id); //Read by Id
+        void Create(T entity); // Create
+        void Update(T entity); // Upadate
+        void Delete(int id); //Delete by Id
+    }
+}
